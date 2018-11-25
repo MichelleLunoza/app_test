ea72cb04232f6e209b61262b8c5852eb6d7aeee5
MichelleLunoza <michelle.lunoza118@gmail.com>
2018-11-25 22:45:32 +0800

    added sass and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require("gulp");
 var pug = require("gulp-pug");
+var sass = require("gulp-sass");
+var minifycss = require("gulp-minify-css");
+var watch = require("gulp-watch");
+
+var paths = {
+  sass: ["./sass/*.sass"]
+};
 
 gulp.task("views", function buildHTML() {
   return gulp
@@ -11,8 +18,28 @@ gulp.task("views", function buildHTML() {
     )
     .pipe(gulp.dest("compiled/"));
 });
+
+/* Compile SASS */
+gulp.task("sass", function(done) {
+  gulp
+    .src("./sass/general.sass")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./css/"))
+    .pipe(
+      minifycss({
+        keepSpecialComments: 0
+      })
+    );
+});
+
+/* Watch Mode */
+gulp.task("watch", ["sass"], function() {
+  gulp.watch(paths.sass, ["sass"]);
+});
+
 // gulp.task("watch", function() {
-//   return watch("views/app.pug", {
+//   return watch("sass/*.sass", {
 //     ignoreInitial: false
 //   }).pipe(gulp.dest("pug"));
 // });
